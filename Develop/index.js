450b0cc91a80fef64bf3a49d570103c8f01403f6
Xandromus <xanderrapstine@gmail.com>
2020-03-18 10:54:06 -0500

    add code comments

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,16 @@
+// array of questions for user
 const questions = [
 
 ];
 
+// function to write README file
 function writeToFile(fileName, data) {
 }
 
+// function to initialize program
 function init() {
 
 }
 
+// function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,4 @@
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.title}
