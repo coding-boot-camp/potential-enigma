015b7547e556788e74bc40fdfc858dae7cb4a82d
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-09-24 17:14:49 -0600

    trimmed the data for the optional sections

--- dist/README.md
+++ dist/README.md
@@ -1,2 +1,9 @@
-# game-finder
+# GAME-FINDER
+
+## Description
+
+  an app to help users determine what game to buy. It also shows you where you can buy the game, and displays reviews.
+  
+
+## Questions
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,35 @@
+const generateSection = sectionArr => {
+
+}
+
+const generateOptSections = data => {
+  let dataArr = [];
+  if (data.confirmEmail) {
+    dataArr = Object.entries(data).slice(5, Object.entries(data).length);
+  } else {
+    dataArr = Object.entries(data).slice(4, Object.entries(data).length);
+  }
+  console.log(dataArr);
+  let sections = ``;
+
+  // for 
+
+  return `
+  `;
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let title = data.title.toUpperCase();
+  return `# ${title}
+
+## Description
+
+  ${data.description}${generateOptSections(data)}
+
+## Questions
 
 `;
-}
+};
 
 module.exports = generateMarkdown;
