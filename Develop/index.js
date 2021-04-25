e4f7e9edff9918f88d09bddd1138a290a7452caf
Your Name <you@example.com>
2021-04-25 17:46:48 -0500

    question set up for readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
-console.log("hi");
+const inquirer = require('inquirer');
+
+const {prompt} = inquirer;
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please include information about your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        messsage: 'Please explain the steps to your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your application?'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'enter applicable license',
+        choices: ['MIT', 'apache-2.0', 'gpl-2.0']
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please contact me! I can be reached at this link.'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
