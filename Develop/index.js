f412ad036378f3f5dea80b324fb969ba9000dff4
anthonycc <anthonyccardozo@gmail.com>
2021-07-25 15:18:04 -0400

    forked starter code, started installing dependencies

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
