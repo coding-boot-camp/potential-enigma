3afa18375ddd37b5cbe9274145bd909e5a59fab5
sbrack8 <sbrack8@gmail.com>
2022-01-11 12:23:35 -0500

    removed console log

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,172 @@
-// TODO: Include packages needed for this application
+#!/usr/bin/env node
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const { truncate, write } = require('fs');
+const inquirer = require('inquirer');
+const { type } = require('os');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const writeFile = require('./utils/generateFile.js');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your repository?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Please enter some information about your project:',
+            validate: aboutInput => {
+                if (aboutInput) {
+                    return true;
+                } else {
+                    console.log('Please enter some information!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstall',
+            message: 'Does your project need special instructions on how to install?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Enter your installation instructions:',
+            when: ({ confirmInstall }) => {
+                if (confirmInstall) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please explain the usage of this project:',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: ['MIT', 'GNU', 'Apache', 'BSD', 'ISC']
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContribute',
+            message: 'Will your project be open to contributions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please explain your contribution guidlines for this project:',
+            when: ({ confirmContribute }) => {
+                if (confirmContribute) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTest',
+            message: 'Will your project need testing instructions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'Please input testing instructions for the user:',
+            when: ({ confirmTest }) => {
+                if (confirmTest) {
+                    return true;
+                } else {
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'What is your GitHub username?',
+            validate: gitInput => {
+                if (gitInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'gitLink',
+            message: 'Please enter your GitHub Profile Link:',
+            validate: gitLinkInput => {
+                if (gitLinkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Link!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email adress for people to contact you:',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email adress!');
+                    return false;
+                }
+            }
+        }
 
-// TODO: Create a function to initialize app
-function init() {}
+    ])
+        .then(data => {
+            return generateMarkdown(data);
+        })
+        .then(pageMarkdown => {
+            return writeFile(pageMarkdown);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+function init() {
+    promptQuestions()
+}
 
-// Function call to initialize app
 init();
+
+
+
+// README must have title of project, description, table of contents, installation, usage, license, contributing, tests, and questions
+// project title projected at top where title is
+// license is a multiple choice option - badge of license type must be added to top of readme along with in license part
+// git hub user name must be added to questions sections with link to github profile
+// email address will also be in questions with instructions on how to reach out with any extra questions
+// table of contents works with links
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,78 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const generateInstall = installText => {
+  if (!installText) {
+    return ''
+  } else {
+    return `## Installation
+  ${installText}
+  `
+  }
+}
+const generateContribute = contributeText => {
+  if (!contributeText) {
+    return ''
+  } else {
+    return `## Contributions
+  ${contributeText}
+  `
+  }
+}
+const generateTesting = testText => {
+  if (!testText) {
+    return ''
+  } else {
+    return `## Testing
+  ${testText}
+  `
+  }
+}
+const installCheck = check => {
+  if (!check) {
+    return '';
+  } else {
+    return `* [Installation](#installation)`
+  }
+}
+const contributeCheck = check => {
+  if (!check) {
+    return '';
+  } else {
+    return `* [Contributions](#contributions)`
+  }
+}
+const testCheck = check => {
+  if (!check) {
+    return '';
+  } else {
+    return `* [Testing](#testing)`
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  ![GitHub](https://img.shields.io/github/license/${data.githubUsername}/${data.title})
+  # ${data.title}
+  ## Description
+  ${data.about}
+  ## Table of Contents
+  ${installCheck(data.install)}
+  * [Usage](#usage)
+  * [License](#license)
+  ${contributeCheck(data.contribution)}
+  ${testCheck(data.testing)}
+  * [Questions](#questions)
+  
+  ${generateInstall(data.install)}
+  ## Usage
+  ${data.usage}
+  ## License
+  * This application is covered under the ${data.license} license
+  ${generateContribute(data.contribution)}
+  ${generateTesting(data.testing)}
+  ## Questions
+  Created by: [${data.githubUsername}](${data.gitLink})
+  
+  If you have any further questions please feel free to contact me at [${data.email}](${data.email})
 `;
 }
 
