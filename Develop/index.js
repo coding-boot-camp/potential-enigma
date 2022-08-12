21b8f2a8ba387091765fd174da6e7e84337fb627
Hickups789 <jackysantiago358@yahoo.com>
2022-08-12 12:15:41 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,87 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const promptQuestions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your repository? (Required)",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter your repository title!");
+          return false;
+        }
+      },
+    }, 
+    {
+      type: "list",
+      name: "license",
+      message: "Which license will you be using for your repo? (Required)",
+      choices: ["No License", "MIT"]
+    },
+     {
+       type: "input",
+      name: "description",
+       message: "What is the description of your repository? (Required)",
+       validate: (descriptionInput) => {
+         if (descriptionInput) {
+           return true;
+         } else {
+          console.log("Please enter description of the repository!");
+          return false;
+         }
+       },
+     },
+     {
+       type: "confirm",
+       name: "confirmInstallation",
+       message: "Is there an installation procedure?",
+       default: true,
+     },
+     {
+      type: "input",
+       name: "installation",
+       message: "Please provide list of installation instructions:",
+       when: ({ confirmInstallation }) => confirmInstallation,
+     },
+     {
+       type: "confirm",
+       name: "confirmUsage",
+      message: "would you like you give instructions for you application?",
+       default: true,
+     },
+     {
+       type: "input",
+       name: "instructions",
+       message: "Please provide application instructions:",
+       when: ({ confirmUsage }) => confirmUsage,
+     },
+  ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+  promptQuestions()
+  .then((inquirerRepsonse) => {
+    console.log(inquirerRepsonse)
+    fs.writeFile('ReadMe.md', generateMarkdown(inquirerRepsonse), err => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    });
+    console.log("Your READme has successfully created!");
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
 
 // Function call to initialize app
 init();
+
