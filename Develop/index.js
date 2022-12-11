46202395c35baf8c2cffbaa542d18b4df1b3b1e1
Andrew <andrewy3295@gmail.com>
2022-12-10 19:47:29 -0600

    Can't get the readme to work

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,110 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+function generateReadme(title, description, table_of_contents, installation, usage, license, contributing, tests, questions){
+    `# <${title}>
+    ## Description
+        ${description}
+    ## Table of Contents
+        ${table_of_contents}
+    ## Installation
+        ${installation}
+    ## Usage
+        ${usage}
+    ## Liscense
+        ${license}
+    ## Contributing
+        ${contributing}
+    ## Tests
+        ${tests}
+    ## Questions
+        ${questions}`
+}
+
+// function Question(title, description, table_of_contents, installation, usage, license, contributing, tests, questions) {
+//     this.title = title;
+//     this.description = description;
+//     this.table_of_contents = table_of_contents;
+//     this.installation = installation;
+//     this.usage = usage;
+//     this.license = license;
+//     this.contributing = contributing;
+//     this.tests = tests;
+//     this.questions = questions;
+//     console.log("Question is working!!!")
+// }
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Provide a description of your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'List key contents to create a table of contents',
+            name: 'table_of_contents',
+        },
+        {
+            type: 'input',
+            message: 'Please provide installation instructions if applicable.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please provide information on how to use the application.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What type of license will you be using for this application?',
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'How would you like other developers to contribute?',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'What are some test for your application and how do you run them?',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'Please provide your Github username.',
+            name: 'questions',
+        },
+    ])
+    .then((info) => {
+        const readmeContent = generateReadme(info)
+        fs.writeFile("README.md", readmeContent, (err) =>
+            err ? console.log(err): console.log("Successfully created your README.md file."))
+    })
+
+
+
+
+
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+WHEN I enter my project title
+THEN this is displayed as the title of the README
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
