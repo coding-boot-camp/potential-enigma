e5618eb62cbe8f53d1e8839cf9bec1580162d52c
Venus Rodin <VenusViolet1234@gmail.com>
2023-02-07 12:49:36 -1000

    finished index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = ({ title, description, tableofcontents, installation }) =>
+const generateHTML = ({ title, description, tableofcontents, installation, usage, license, contributing, tests, questions }) =>
   `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,12 +16,17 @@ const generateHTML = ({ title, description, tableofcontents, installation }) =>
 <body>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Hi! The name of my README Repository is ${title}</h1>
+    <h1 class="display-4">Hi! The name of the README Repository is ${title}</h1>
     <p class="lead"> ${description}.</p>
     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
     <ul class="list-group">
-      <li class="list-group-item">My table of contents is ${tableofcontents}</li>
-      <li class="list-group-item"> What is your installation process? ${installation}</li>
+      <li class="list-group-item">The table of contents is ${tableofcontents}</li>
+      <li class="list-group-item">The installation process is ${installation}</li>
+      <li class="list-group-item">The usage of this application is ${usage}</li>
+      <li class="list-group-item">${license}</li>
+      <li class="list-group-item"> The contributers are ${contributing}</li>
+      <li class="list-group-item">This application can be tested by ${tests}</li>
+      <li class="list-group-item">Any extra ${questions}?</li>
     </ul>
   </div>
 </div>
@@ -38,17 +43,7 @@ inquirer
     {
       type: 'input',
       name: 'description',
-      message: 'Whhat is the description of your README?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      message: 'What is the description of your README?',
     },
     {
       type: 'input',
@@ -60,6 +55,31 @@ inquirer
       name: 'installation',
       message: 'Enter your installation process.',
     },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage of your application.',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'What is the license?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Are there any contributers?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What are your tests instructions?',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Instructions on how to reach you with any questions.',
+      },
   ])
   .then((answers) => {
     const htmlPageContent = generateHTML(answers);
@@ -70,10 +90,19 @@ inquirer
   });
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeToFileSync(path.join(process.cwd(), fileName),data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const responses = await inquirer.prompt(questions);
+    const data = markdown({...responses});
+
+    console.log('Creating README...');
+
+    writeToFile('README.md', data);
+}
 
 // Function call to initialize app
 init();
