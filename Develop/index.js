695fb0a69d7c8fc2222af9a9715a5f331c7d0000
Venus Rodin <VenusViolet1234@gmail.com>
2023-02-06 14:18:03 -1000

    began assignment to meet the grading qualifications

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateHTML = ({ title, description, tableofcontents, installation }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! The name of my README Repository is ${title}</h1>
+    <p class="lead"> ${description}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My table of contents is ${tableofcontents}</li>
+      <li class="list-group-item"> What is your installation process? ${installation}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your unique title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Whhat is the description of your README?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'tableofcontents',
+      message: 'Enter your table of contents',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter your installation process.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
