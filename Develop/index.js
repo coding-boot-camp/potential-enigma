6e933ea96f515060e11f9a723a09acb4e398f352
Alan Hornbaker <alanhornbaker@gmail.com>
2023-02-15 16:24:08 -0600

    Created MVP. Issues for development: add images promopt, validation to questions.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your application?",
+  },
+  {
+    type: "input",
+    name: "purpose",
+    message:
+      "What is the intended purpose of the application? What problem is it trying to solve?",
+  },
+  {
+    type: "input",
+    name: "install",
+    message:
+      "How can a user install the applicaiton and make it ready for use?",
+  },
+  {
+    type: "input",
+    name: "use",
+    message: "How does the user use the application once it is installed?",
+  },
+  {
+    type: "input",
+    name: "issues",
+    message: "How can the user report errors or problems?",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "How can the user contribute to the project?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const content = `
+  # ${data.title}
+  
+  ## Description
+  ${data.purpose}
+  
+  ## Installation
+  ${data.install}
+  
+  ## Usage
+  ${data.use}
+  
+  ## Reporting Issues
+  ${data.issues}
+  
+  ## Contributions
+  ${data.contributions}
+    `;
+  fs.writeFile(fileName, content, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`README.md file generated successfully`);
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", data);
+  });
+}
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# Professional README Generator Starter Code
+# Professional README Generator
+
+## Motivation for Project
+
+When creating an open source project on GitHub, it’s important to have a high-quality README for the app. This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions—this last part increases the likelihood that other developers will contribute to the success of the project.
+
+Having a README Generator will allow the developer to save time writing the document. The README Generator will also make sure important information isn't missing from the file, and make sure the information is displayed in an accessable, logical way, so that other developers spend less time in confusion when trying to maintain a project.
+
+I am personally building this project because I suck at writing README files, and I find it boring; I want to be spending my time writing code and not drafting explanations, etc. However, as a developer, I want to be easy to collaborate with so that people don't hate working with me.
+
+## Installation and Use
+
+## Errors and Issues
+
+##Ideas for Future Development
+
+1. There is no validate function in the questions array to require the user to provide input.
+2. Sometimes it is helpful to have pictures in the README. I would like to add a prompt asking the user for file paths for images they might want to include, prompt for a title, and then have that appear in the generated README.
+
+## How to Contribute
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
